7858acb7d62aa2e501ccf3c1b45457bd69722932
abalvaro1 <ame2155006@maricopa.edu>
2023-09-14 19:34:58 -0700

    initial commit

---
+++ src/main/java/org/example/carddeck/Card.java
@@ -0,0 +1,15 @@
+package org.example.carddeck;
+
+public class Card {
+
+    private Suit suit;
+    private FaceValue value:
+
+    public Card(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Card(FaceValue value) {
+        this.value = value;
+    }
+}
